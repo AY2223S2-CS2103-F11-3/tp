ab7afcc3b3f0e440bad83dc8f356b487c2a88534
daitenshionyan <100074448+daitenshionyan@users.noreply.github.com>
2023-02-24 16:08:28 +0800

    Created untested class

---
+++ src/main/java/seedu/vms/Untested.java
@@ -0,0 +1,14 @@
+package seedu.vms;
+
+
+/** An untested class. */
+public class Untested {
+    public int getNumber() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 10) {
+                return 0;
+            }
+        }
+        return 0;
+    }
+}
